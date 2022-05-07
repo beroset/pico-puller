021b7858b64b8996088c79abf1d1f7255c62903e
Ed Beroset <beroset@ieee.org>
2022-05-07 13:53:51 -0400

    Add sample scripts
    This adds the dump and readlocks command files to show what they look
    like and give the user some useful tools.

---
+++ scripts/dump.cmd
@@ -0,0 +1,7 @@
+help
+set_speed 2.5e6
+reset 0
+status
+id 0 0 0 0 0 0 0
+status
+dump
---
+++ scripts/readlocks.cmd
@@ -0,0 +1,9 @@
+reset 0
+status
+lockread cffff
+lockread dffff
+lockread effff
+lockread f7fff
+lockread f9fff
+lockread fbfff
+lockread fffff
